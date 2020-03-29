25da5bb194da9dc5c9a4c9d6e5841e3989248a19
nori1994 <313.hy.sss@gmail.com>
2020-03-29 10:14:25 +0900

    画像保存不可対応

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,28 @@
+function imgGuard(name) {
+  //PCの右クリック対応
+  $(name).on('contextmenu', function (e) {
+    return false;
+  });
+
+  $(name).on('mousedown mouseup', function (e) {
+    return false;
+  });
+
+  //スマホ長押し対応
+  $(name).css({
+    'pointer-events': 'none',
+    '-webkit-touch-callout': 'none',
+    '-webkit-user-select': 'none',
+    '-moz-touch-callout': 'none',
+    '-moz-user-select': 'none',
+    'touch-callout': 'none',
+    'user-select': 'none'
+  });
+}
+
 $(function () {
+  imgGuard('.imgGuard');
+
   var $nav = $('#gnav');
   var navHeight = $nav.innerHeight();
 
